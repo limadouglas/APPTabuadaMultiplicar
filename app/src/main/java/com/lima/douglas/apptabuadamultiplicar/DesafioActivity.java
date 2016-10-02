255d429b40737fc8b12139faf7358771fc5640f0
limadouglas <lima.dougglas@gmail.com>
2016-10-01 21:00:00 -0300

    Iniciando codificação do menu desafio

--- app/src/main/java/com/lima/douglas/apptabuadamultiplicar/DesafioActivity.java
+++ app/src/main/java/com/lima/douglas/apptabuadamultiplicar/DesafioActivity.java
@@ -4,9 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 
 public class DesafioActivity extends AppCompatActivity {
+    
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
