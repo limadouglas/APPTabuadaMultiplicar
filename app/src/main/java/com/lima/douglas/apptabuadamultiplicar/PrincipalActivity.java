e1695f0b73785e84782a2c711481344d106d10f5
Douglas Henrique <lima.dougglas@gmail.com>
2016-10-27 17:32:22 -0200

    iniciando implementação de um toobar customizado.

--- app/src/main/java/com/lima/douglas/apptabuadamultiplicar/PrincipalActivity.java
+++ app/src/main/java/com/lima/douglas/apptabuadamultiplicar/PrincipalActivity.java
@@ -15,6 +15,7 @@ public class PrincipalActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.principal_activity);
+        getSupportActionBar().hide();
     }
 
     // chamando activity.
