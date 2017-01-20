279be344b5d7488bc2d8d094de002e97d61d5063
Douglas Henrique <lima.dougglas@gmail.com>
2017-01-19 23:54:05 -0200

    Ativação das propagandas, sim elas estavam em modo teste este tempo todo  : -(  .

--- app/src/main/java/com/lima/douglas/apptabuadamultiplicar/util/Constantes.java
+++ app/src/main/java/com/lima/douglas/apptabuadamultiplicar/util/Constantes.java
@@ -4,7 +4,7 @@ public class Constantes {
     public static final String BD_NOME = "BD_RECORDES"; //nome do banco de dados.
     public static final int BD_VERSION = 1;         // versao do banco de dados.
     public static final String APP_KEY = "6f0f6e900ac2fc0f797cb849cf66628c2f8e9a01eccf474e"; // numero da chave do appodeal.
-    public static boolean TESTEAPPODEAL = true; // ativar modo teste de propaganda.
+    public static boolean TESTEAPPODEAL = false; // ativar modo teste de propaganda.
     public static int QTD_PROPAGANDA = 3; // ativar propaganda.
     public static int PROPAGANDA = 0; // ativar propaganda.
 }
